109d9cd3bf7ec71fc6e2cf91075c07d0532dfb76
kitibayuni <phmacaril@gmail.com>
2025-08-29 16:12:58 -0400

    FIX: fixed spacing typo for error codes (-mc) in ffuf_subdomain.sh script, and arguments provided from 2 -> 3

--- ffuf/ffuf_subdomain.sh
+++ ffuf/ffuf_subdomain.sh
@@ -3,7 +3,7 @@
 # FFUF subdomain enumeration
 
 # check for domain and worlist args
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 3 ]; then
 	echo "Usage: $0 <domain> <wordlist> <protocol>"
 	echo "Example: ./ffuf_subdomain.sh example.com rockyou.txt https"
 	exit 1
@@ -14,4 +14,4 @@ WORDLIST="$2"
 PROTOCOL="$3"
 OUTPUT="ffuf_subdomains_$DOMAIN.json"
 
-ffuf -w "$WORDLIST" -u "$PROTOCOL://FUZZ.$DOMAIN" -H "Host: FUZZ.$DOMAIN" -mc 200, 301, 302 -t 50 -o "$OUTPUT" -of json
+ffuf -w "$WORDLIST" -u "$PROTOCOL://FUZZ.$DOMAIN" -H "Host: FUZZ.$DOMAIN" -mc 200,301,302 -t 50 -o "$OUTPUT" -of json
