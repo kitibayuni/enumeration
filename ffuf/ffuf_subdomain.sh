6d441d6943afbc20479264fb1aed93bf944b30a8
kitibayuni <phmacaril@gmail.com>
2025-08-29 15:49:11 -0400

    ADD: new ffuf section; created simple ffuf subdomain enumeration script

---
+++ ffuf/ffuf_subdomain.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# FFUF subdomain enumeration
+
+# check for domain and worlist args
+if [ "$#" -ne 2 ]; then
+	echo "Usage: $0 <domain> <wordlist> <protocol>"
+	echo "Example: ./ffuf_subdomain.sh example.com rockyou.txt https"
+	exit 1
+fi
+
+DOMAIN="$1"
+WORDLIST="$2"
+PROTOCOL="$3"
+OUTPUT="ffuf_subdomains_$DOMAIN.json"
+
+ffuf -w "$WORDLIST" -u "$PROTOCOL://FUZZ.$DOMAIN" -H "Host: FUZZ.$DOMAIN" -mc 200, 301, 302 -t 50 -o "$OUTPUT" -of json
